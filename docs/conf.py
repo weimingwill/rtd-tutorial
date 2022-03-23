9cc33d92cdc424cda379c790ad68fd85846582ab
Weiming <wingalong@gmail.com>
2022-03-23 16:55:32 +0800

    update

--- docs/conf.py
+++ docs/conf.py
@@ -89,8 +89,7 @@ html_theme_options = {
         },
     ],
     # Specify the language of shared menu
-    'menu_lang':
-    'en'
+    'menu_lang': 'en'
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
